12220629072a74fa215a607e7f82e3dd0b09a992
sggyflt <sgflt88@gmail.com>
2024-03-28 23:20:54 +0100

    feat: add CountrySelect with the useCountries hook

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -3,6 +3,7 @@
 import useRentModal from "@/app/hooks/useRentModal";
 import CategoryInput from "../inputs/CategoryInput";
 import Heading from "../Heading";
+import CountrySelect from "../inputs/CountrySelect";
 import Modal from "./Modal"
 import { useMemo, useState } from "react";
 import { categories } from "../navbar/Categories";
@@ -97,11 +98,23 @@ const RentModal = () => {
     </div>
   );
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+        title="Where is your place located?"
+        subtitle="Help guests find you!"
+        />
+        <CountrySelect />
+      </div>
+    )
+  }
+
   return (
     <Modal 
       isOpen={rentModal.isOpen} 
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
