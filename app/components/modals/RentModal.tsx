b36abc11601d461ed0970bd40b230f7e37167aed
sggyflt <sgflt88@gmail.com>
2024-03-28 23:29:11 +0100

    feat: country selected option is saved

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -46,6 +46,7 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -105,7 +106,10 @@ const RentModal = () => {
         title="Where is your place located?"
         subtitle="Help guests find you!"
         />
-        <CountrySelect />
+        <CountrySelect 
+        value={location}
+        onChange={(value) => setCustomValue("location", value)}
+        />
       </div>
     )
   }
